def12d87b3bc54257b6e8de3e41e219b1a642950
halfordC <wcrossno@uwm.edu>
2022-04-19 22:23:58 -0500

    my _context is Null? strange

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
